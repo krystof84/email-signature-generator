b5c09dbc3ced3ddb8d2b1b5a3aae58357d17da4d
Krzysztof <krisczajka@gmail.com>
2019-04-22 11:20:18 +0200

    test

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+
+const App = () => (
+  // <Router>
+  //   <Navigation />
+  // </Router>
+  <h1>ss</h1>
+);
+
+export default App;
